3235d1c8a0952326900ebd7c36e0a303bc7e0eb8
julianvanost <julian.vanost@gmail.com>
2019-05-22 18:23:49 -0700

    Add styles.js for styling tabs and perhaps other future uses. Moves styling from App.js to styles.js.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import SimpleTab from 'react-simple-tab';
 import { render } from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
+import Styles from './styles';
 window.React = require('react');
 
 class App extends Component {
@@ -26,36 +27,6 @@ class App extends Component {
     );
   }
 }
-
-const Styles = {
-  tabContent: {
-    border: 'solid 2px #ebeced',
-    padding: '15px',
-  },
-  app: {
-    tabStyle: {
-      fontSize: '20px',
-    },
-    labelListStyle: {
-      margin: '2px',
-      padding: 0,
-      bottom: '-2px',
-      position: 'relative',
-    },
-    tabContentStyle: {
-      padding: '5px 8px 0px 8px',
-    },
-    activeTabContentStyle: {
-      borderWidth: '1px 1px 4px 1px',
-      margin: '-4px -1px 0px -1px',
-      borderColor: '#c9c9c9 #c9c9c9 #ff6c60 #c9c9c9 ' ,
-      borderStyle: 'solid',
-      borderRadius: '5px 5px 0px 0px',
-      zIndex: 1,
-      background: 'white',
-    }
-  }
-};
 
 class TabContent extends Component {
   render() {
